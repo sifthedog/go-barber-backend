ce5baf1b29f8ab3337c34091ef56f3ab3d427849
João <joaopfernandesc@gmail.com>
2020-09-03 22:02:36 -0300

    Development email sending

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -31,8 +31,11 @@ export default class SendForgotPasswordEmailService {
       throw new AppError('User does not exists');
     }
 
-    await this.userTokensRepository.generate(user.id);
+    const token = await this.userTokensRepository.generate(user.id);
 
-    this.mailProvider.sendMail(email, 'Pedido de recuperação de senha');
+    await this.mailProvider.sendMail(
+      email,
+      `Pedido de recuperação de senha ${token}`,
+    );
   }
 }
